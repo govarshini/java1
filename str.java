6b38f4bed0afe668d77f25fd48355bc4ffb83a86
govarshini <govarshiniarumugam@gmail.com>
2017-06-12 15:15:02 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class str
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		int b=a.length();
+		for(int i=0;i<b;i++)
+		System.out.println(b);
+	}
+}
